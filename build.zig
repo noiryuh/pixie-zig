82e9b9ed1e4916306ae1f963866840c607c3ebaa
noiryuh <noiryuh@proton.me>
2022-08-07 11:35:24 +0700

    update build.zig

--- build.zig
+++ build.zig
@@ -9,3 +9,28 @@ fn getRootPath() []const u8 {
 pub fn build(b: *std.build.Builder) void {
     _ = b;
 }
+
+// ========================================
+// Package SDK
+// ========================================
+
+pub const pkg = std.build.Pkg{
+    .name = "pixie-zig",
+    .source = .{ .path = root_path ++ "/src/main.zig" },
+};
+
+pub fn linkPkg(_: *std.build.Builder, exe: *std.build.LibExeObjStep, option: LinkPkgOption) void {
+    if (!exe.is_linking_libc) {
+        exe.linkLibC();
+    }
+
+    exe.addLibraryPath(option.path);
+    exe.linkSystemLibrary("pixie");
+
+    exe.addPackage(pkg);
+}
+
+pub const LinkPkgOption = struct {
+    /// Path to library
+    path: []const u8,
+};
