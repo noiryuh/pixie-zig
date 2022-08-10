9007c26d119ab3341285e4339dd7b521bd489399
noiryuh <noiryuh@proton.me>
2022-08-11 02:06:05 +0700

    use abbreviation

--- example/square.zig
+++ example/square.zig
@@ -2,19 +2,19 @@ const std = @import("std");
 const pixie = @import("pixie-zig");
 
 pub fn main() !void {
-    var image = pixie.Image.init(200, 200);
-    defer image.deinit();
-    image.fill(.{ .r = 1, .g = 1, .b = 1, .a = 1 });
+    var img = pixie.Image.init(200, 200);
+    defer img.deinit();
+    img.fill(.{ .r = 1, .g = 1, .b = 1, .a = 1 });
 
-    var context = pixie.Context.initFromImage(image);
-    defer context.deinit();
+    var ctx = pixie.Context.initFromImage(img);
+    defer ctx.deinit();
 
     var paint = pixie.Paint.init(.solid);
     defer paint.deinit();
     paint.setColor(.{ .r = 1, .g = 0, .b = 0, .a = 1 });
 
-    context.setFillStyle(paint);
-    context.fillRect(50, 50, 100, 100);
+    ctx.setFillStyle(paint);
+    ctx.fillRect(50, 50, 100, 100);
 
-    image.writeToFile("square.png");
+    img.writeToFile("square.png");
 }
